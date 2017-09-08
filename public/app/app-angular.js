0394426dc25cc84fee2a1b05e0eb5b8ff62d58f7
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-09-08 12:38:28 +0530

    UI

---
+++ public/app/app-angular.js
@@ -0,0 +1,9 @@
+var app = angular.module('MyApp',[]);
+app.run(function(){
+    console.log("My App is Running!");
+});
+
+
+app.controller('mainCont', function($scope, $http, $location) {
+    
+});
