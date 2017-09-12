217f7d6c40ef90498c8d30890c344afd2e793bce
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-09-12 16:10:41 +0530

    Window

--- server.js
+++ server.js
@@ -107,11 +107,12 @@ restService.post('/inputmsg', function(req, res) {
         GetAuth( req, res, function( req, res, rowCount ){
             if ( rowCount == 0 ) {
                 speech = "Hi! My name is VIKI (Virtual Interactive Kinetic Intelligence) and I am here to help! \nPlease Login @ https://vikii.herokuapp.com/login?id=" + userid;
-                return res.json({
-                    speech: speech,
-                    displayText: speech,
-                    source: 'webhook-OSC-oppty'
-                });
+                var myWindow = window.open("https://vikii.herokuapp.com/login?id=" + userid, "_blank", "width=500, height=500");
+//                return res.json({
+//                    speech: speech,
+//                    displayText: speech,
+//                    source: 'webhook-OSC-oppty'
+//                });
             }
             else if( rowCount == 1 ){
                     
