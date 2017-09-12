dad142af7ffec5d10521663890295b37cd90264d
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-09-12 16:07:00 +0530

    Error MSG

--- public/app/app-angular.js
+++ public/app/app-angular.js
@@ -34,7 +34,6 @@ app.controller('mainCont', function($scope, $http, $location) {
                     $scope.loginerror = false;
                     $location.path('\success');
                     $scope.loginerror = "";
-                    window.close();
                 }
                     
                 else{
