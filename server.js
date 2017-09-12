09fb98a8bea6cd53d3d2b6807106b190cab91ff9
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-09-12 16:16:28 +0530

    Window

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ restService.post('/inputmsg', function(req, res) {
             if ( rowCount == 0 ) {
                 speech = "Hi! My name is VIKI (Virtual Interactive Kinetic Intelligence) and I am here to help! \nPlease Login @ https://vikii.herokuapp.com/login?id=" + userid;
                 var opn = require('opn');
-                opn("https://vikii.herokuapp.com/login?id=" + userid");
+                opn("https://vikii.herokuapp.com/login?id=" + userid);
                 
                 return res.json({
                     speech: speech,
