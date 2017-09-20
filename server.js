eb42c3f280e213c8a40e30942cf986fcd1fac3d3
Gokul Nair <gokulgnair94@gmail.com>
2017-09-20 19:00:12 +0530

    Carsd

--- server.js
+++ server.js
@@ -135,53 +135,7 @@ restService.post('/inputmsg', function(req, res) {
                                                 }]
                                             }
                                         }
-                                    ],
-                                    "carouselSelect": {
-                                    "items": [{
-                                            "optionInfo": {
-                                                "key": "MATH_AND_PRIME",
-                                                "synonyms": [
-                                                    "math",
-                                                    "math and prime",
-                                                    "prime numbers",
-                                                    "prime"
-                                                ]
-                                            },
-                                            "title": "Math & prime numbers",
-                                            "description": "42 is an abundant number because the sum of its proper divisors 54 is greater…",
-                                            "image": {
-                                            }
-                                        },
-                                        {
-                                            "optionInfo": {
-                                                "key": "EGYPT",
-                                                "synonyms": [
-                                                    "religion",
-                                                    "egpyt",
-                                                    "ancient egyptian"
-                                                ]
-                                            },
-                                            "title": "Ancient Egyptian religion",
-                                            "description": "42 gods who ruled on the fate of the dead in the afterworld. Throughout the under…",
-                                            "image": {
-                                            }
-                                        },
-                                        {
-                                            "optionInfo": {
-                                                "key": "RECIPES",
-                                                "synonyms": [
-                                                    "recipes",
-                                                    "recipe",
-                                                    "42 recipes"
-                                                ]
-                                            },
-                                            "title": "42 recipes with 42 ingredients",
-                                            "description": "Here's a beautifully simple recipe that's full of flavor! All you need is some ginger and…",
-                                            "image": {
-                                            }
-                                        }
                                     ]
-                                }
                                 }
                             }
                         }
