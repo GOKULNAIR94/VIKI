ce46f61537064406f8374bbd64b704b3841490a4
Gokul Nair <gokulgnair94@gmail.com>
2017-09-26 15:23:13 +0530

    Activity Sales

--- server.js
+++ server.js
@@ -111,57 +111,39 @@ restService.post('/inputmsg', function(req, res) {
                 var returnJson;
                 if (req.body.originalRequest.source == "google") {
                     returnJson = {
-//                        speech: speech,
-//                        displayText: speech,
-                        
-                            
-                                
-                                "expectUserResponse": true,
-                                "expectedInputs": [{
-                                    "inputPrompt": {
-                                        "richInitialPrompt": {
-                                            "items": [{
-                                                "simpleResponse": {
-                                                    "textToSpeech": "Howdy! I can tell you fun facts about almost any number, like 42. What do you have in mind?",
-                                                    "displayText": "Howdy! I can tell you fun facts about almost any number. What do you have in mind?"
-                                                }
-                                            }],
-                                            "suggestions": []
+                        speech: speech,
+                        displayText: speech,
+                        data : {
+                            google: {
+                                'expectUserResponse': true,
+                                'isSsml': false,
+                                'noInputPrompts': [],
+                                'richResponse': {
+                                    'items': [{
+                                            'simpleResponse': {
+                                                'textToSpeech': 'Hi! My name is VIKI (Virtual Interactive Kinetic Intelligence) and I am here to help! Please click the below button to Login!',
+                                                'displayText': 'Hi! My name is VIKI (Virtual Interactive Kinetic Intelligence) and I am here to help!'
+                                            }
+                                        },
+                                        {
+                                            'basicCard': {
+                                                'title': '',
+                                                "image": {
+                                                    "url": "",
+                                                    "accessibilityText": ""
+                                                },
+                                                'buttons': [{
+                                                    'title': 'Login to Viki',
+                                                    'openUrlAction': {
+                                                        'url': "https://vikii.herokuapp.com/login?id=" + userid
+                                                    }
+                                                }]
+                                            }
                                         }
-                                    },
-                                    "possibleIntents": [{
-                                        "intent": "actions.intent.TEXT"
-                                    }]
-                                }]
-//                                'expectUserResponse': true,
-//                                'isSsml': false,
-//                                'noInputPrompts': [],
-//                                'richResponse': {
-//                                    'items': [{
-//                                            'simpleResponse': {
-//                                                'textToSpeech': 'Hi! My name is VIKI (Virtual Interactive Kinetic Intelligence) and I am here to help! Please click the below button to Login!',
-//                                                'displayText': 'Hi! My name is VIKI (Virtual Interactive Kinetic Intelligence) and I am here to help!'
-//                                            }
-//                                        },
-//                                        {
-//                                            'basicCard': {
-//                                                'title': '',
-//                                                "image": {
-//                                                    "url": "",
-//                                                    "accessibilityText": ""
-//                                                },
-//                                                'buttons': [{
-//                                                    'title': 'Login to Viki',
-//                                                    'openUrlAction': {
-//                                                        'url': "https://vikii.herokuapp.com/login?id=" + userid
-//                                                    }
-//                                                }]
-//                                            }
-//                                        }
-//                                    ]
-//                                }
-                            
-                        
+                                    ]
+                                }
+                            }
+                        }
                     }
                 }
                 else{
